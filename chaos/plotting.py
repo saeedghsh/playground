c589c939d968a63d481a3aaeb0409e6b29ecd629
Saeed Gholami Shahbandi <2572507+saeedghsh@users.noreply.github.com>
2020-07-23 21:23:54 +0200

    Clean up
    Clean up after setting up formatter and linter in Makefile.

--- chaos/plotting.py
+++ chaos/plotting.py
@@ -6,11 +6,11 @@ SlidingFigure: An object to plot the "logistic map" curve and
 
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.widgets
 
 from . import logisticmap
+
 
 class SlidingFigure:
     """A plotting object
@@ -38,36 +38,37 @@ class SlidingFigure:
 
         self._fig = plt.figure(figsize=(18, 6))
         self._axes = [
-            plt.subplot2grid((9, 4), (0, 0), rowspan=8, colspan=1), # logistic_map
-            plt.subplot2grid((9, 4), (8, 0), rowspan=1, colspan=4), # slider
-            plt.subplot2grid((9, 4), (0, 1), rowspan=8, colspan=3), # chaotic processes
+            plt.subplot2grid((9, 4), (0, 0), rowspan=8, colspan=1),  # logistic_map
+            plt.subplot2grid((9, 4), (8, 0), rowspan=1, colspan=4),  # slider
+            plt.subplot2grid((9, 4), (0, 1), rowspan=8, colspan=3),  # chaotic processes
         ]
         self._plot_logistic_map(self._axes[0])
         self._plot_chaotic_processes(self._axes[2])
         self._r_slider = matplotlib.widgets.Slider(
-            self._axes[1], 'r',
-            0.0, 4.0, valinit=self._lm.r, valstep=0.2)
+            self._axes[1], "r", 0.0, 4.0, valinit=self._lm.r, valstep=0.2
+        )
         self._r_slider.on_changed(self.update)
 
         self._fig.tight_layout()
         plt.show()
 
     def _plot_logistic_map(self, axis):
-        self._lm_curve_plot = axis.plot( self._lm.XY[:,0], self._lm.XY[:,1],
-                                         label='logistic map')[0]
-        axis.plot([0, 1], [0, 1], 'k', label='linear')
-        axis.set_title('logistic map vs. linear')
-        axis.set_xlabel('x[t-1]')
-        axis.set_ylabel('x[t]')
-        axis.axis('equal')
-        axis.legend(loc='upper left', fancybox=True, shadow=False, framealpha=0.6)
+        self._lm_curve_plot = axis.plot(
+            self._lm.XY[:, 0], self._lm.XY[:, 1], label="logistic map"
+        )[0]
+        axis.plot([0, 1], [0, 1], "k", label="linear")
+        axis.set_title("logistic map vs. linear")
+        axis.set_xlabel("x[t-1]")
+        axis.set_ylabel("x[t]")
+        axis.axis("equal")
+        axis.legend(loc="upper left", fancybox=True, shadow=False, framealpha=0.6)
 
     def _plot_chaotic_processes(self, axis):
-        self._lm_processes_plot = axis.plot(self._lm.X, 'k', linewidth=0.3, alpha=0.6)
-        title = '{:d} chaotic processes (with random initial points) generated from the logistic map'
+        self._lm_processes_plot = axis.plot(self._lm.X, "k", linewidth=0.3, alpha=0.6)
+        title = "{:d} chaotic processes (with random initial points) generated from the logistic map"
         axis.set_title(title.format(self._lm.X.shape[1]))
-        axis.set_xlabel('t')
-        axis.set_ylabel('x[t]')
+        axis.set_xlabel("t")
+        axis.set_ylabel("x[t]")
 
     def update(self, val):
         """
