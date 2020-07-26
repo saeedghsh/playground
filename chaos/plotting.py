3dce701d6b59859a0594f9d6513cd153c2c1c689
Saeed Gholami Shahbandi <2572507+saeedghsh@users.noreply.github.com>
2020-07-26 14:48:52 +0200

    Add frequncy spectrum plot

--- chaos/plotting.py
+++ chaos/plotting.py
@@ -5,9 +5,9 @@ SlidingFigure: An object to plot the "logistic map" curve and
     "logistic map"
 
 """
-
 import matplotlib.pyplot as plt
 import matplotlib.widgets
+from scipy import fftpack
 
 from . import logisticmap
 
@@ -36,14 +36,18 @@ class SlidingFigure:
         assert isinstance(logistic_map, logisticmap.LogisticMap)
         self._lm = logistic_map
 
-        self._fig = plt.figure(figsize=(18, 6))
+        self._fig = plt.figure(figsize=(18, 8))
         self._axes = [
             plt.subplot2grid((9, 4), (0, 0), rowspan=8, colspan=1),  # logistic_map
             plt.subplot2grid((9, 4), (8, 0), rowspan=1, colspan=4),  # slider
-            plt.subplot2grid((9, 4), (0, 1), rowspan=8, colspan=3),  # chaotic processes
+            plt.subplot2grid((9, 4), (0, 1), rowspan=4, colspan=3),  # chaotic processes
+            plt.subplot2grid(
+                (9, 4), (4, 1), rowspan=4, colspan=3
+            ),  # frequency spectrum
         ]
         self._plot_logistic_map(self._axes[0])
         self._plot_chaotic_processes(self._axes[2])
+        self._plot_frequency_spectrum(self._axes[3])
         self._r_slider = matplotlib.widgets.Slider(
             self._axes[1], "r", 0.0, 4.0, valinit=self._lm.r, valstep=0.2
         )
@@ -71,6 +75,14 @@ class SlidingFigure:
         axis.set_xlabel("t")
         axis.set_ylabel("x[t]")
 
+    def _plot_frequency_spectrum(self, axis):
+        X = fftpack.fft(self._lm.X[:, 0])
+        self._lm_spectrum_plot = axis.plot(X, "k", linewidth=0.3, alpha=0.6)[0]
+        axis.set_title("frequency spectrum")
+        axis.set_ylim([X.real.min() - 1, X.real.max() + 1])
+        axis.set_xlabel("f")
+        axis.set_ylabel("spectrum")
+
     def update(self, val):
         """
         Parameters
@@ -80,9 +92,17 @@ class SlidingFigure:
         """
 
         self._lm.r = self._r_slider.val
+
         self._lm_curve_plot.set_ydata(self._lm.XY[:, 1])
+
         for i, p in enumerate(self._lm_processes_plot):
             p.set_ydata(self._lm.X[:, i])
+        p.axes.set_ylim([0, self._lm.X.max()])
+
+        X = fftpack.fft(self._lm.X[:, 0])
+        self._lm_spectrum_plot.set_ydata(X)
+        self._lm_spectrum_plot.axes.set_ylim([X.real.min() - 1, X.real.max() + 1])
+
         self._fig.canvas.draw_idle()
 
         # # for saving plots and creating animated gifs
