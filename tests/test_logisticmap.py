ca9cc348168d81703cb4b484ec83b62894f05c38
Saeed Gholami Shahbandi <2572507+saeedghsh@users.noreply.github.com>
2020-07-26 11:59:50 +0200

    Add more test, fully cover logisticmap

--- tests/test_logisticmap.py
+++ tests/test_logisticmap.py
@@ -1,13 +1,15 @@
 """Test for the LogisticMap class"""
 
+import numpy as np
+
 # to be able to import `chaos.logisticmap`, it is important that this
 # directory (test) contains the `__init__.py`
-from chaos.logisticmap import LogisticMap
+from chaos.logisticmap import LogisticMap, logistic_map
 
 R = 2.0
-COUNT = 4
+COUNT = 3
 LENGTH = 6
-X0 = [0.25, 0.5, 0.75]
+X0 = [0.25, 0.50, 0.75]
 
 
 def initiate_logistic_map():
@@ -15,11 +17,23 @@ def initiate_logistic_map():
 
 
 class TestLogisticMap:
-    def test_init(self):
+    def test___init__(self):
         lm = initiate_logistic_map()
         assert lm._r == R
         assert lm._count == COUNT
         assert lm._length == LENGTH
+
+    def test___str__(self):
+        lm = initiate_logistic_map()
+        assert lm.__str__() == "LogisticMap(r=2.00, x0=[0.25, 0.50, 0.75])"
+
+    def test___repr__(self):
+        lm = initiate_logistic_map()
+        assert lm.__repr__() == "LogisticMap(r=2.00, x0=[0.25, 0.50, 0.75])"
+
+    def test___len__(self):
+        lm = initiate_logistic_map()
+        assert len(lm) == 3
 
     def test_get_r(self):
         lm = initiate_logistic_map()
@@ -40,16 +54,31 @@ class TestLogisticMap:
         assert lm._X0 == [x0 + 0.1 for x0 in X0]
 
     def test_get_X(self):
-        assert True
+        lm = initiate_logistic_map()
+        assert lm.X.shape == (6, 3)
 
     def test_get_XY(self):
-        assert True
+        x = np.linspace(0, 1, 100)
+        y = logistic_map(R, x)
+        lm = initiate_logistic_map()
+        assert (lm.XY == np.array([x, y]).T).all()
 
     def test__reset(self):
-        assert True
+        lm = initiate_logistic_map()
+        assert lm.X.shape == (6, 3)
+        lm._reset()
+        assert lm.X.shape == (1, 3)
 
     def test__next(self):
-        assert True
+        lm = initiate_logistic_map()
+        assert lm.X.shape == (6, 3)
+        lm._reset()
+        lm._next(3)
+        assert lm.X.shape == (4, 3)
 
     def test_scramble(self):
-        assert True
+        lm = initiate_logistic_map()
+        X = lm.X.copy()
+        lm.scramble()
+        assert X.shape == lm.X.shape == (6, 3)
+        assert (lm.X != X).all()
