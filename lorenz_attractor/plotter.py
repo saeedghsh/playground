8e478b3cd0c5b6055e4fe36156fd7adb0db69b16
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2024-05-31 17:04:16 +0200

    Lorenz: fix import sort
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- lorenz_attractor/plotter.py
+++ lorenz_attractor/plotter.py
@@ -2,8 +2,8 @@
 # pylint: disable=missing-function-docstring
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def plot_lorenz_attractor(solution_points: np.ndarray):
