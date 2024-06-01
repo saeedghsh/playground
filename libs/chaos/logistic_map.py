8b78a87b9a9df460c77cfdd185fa744dc419ce0f
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2024-06-01 21:41:10 +0200

    logistic_map: some clean up and refactoring
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- libs/chaos/logistic_map.py
+++ libs/chaos/logistic_map.py
@@ -1,9 +1,4 @@
-"""This module provides objects for chaotic processes
-
-Currently it only contains one object, namely "LogisticMap"
-"""
-
-from typing import List
+"""This module provides objects for chaotic processes"""
 
 import numpy as np
 
@@ -18,16 +13,13 @@ def _cobweb(r: float, x0: float, length: int) -> np.ndarray:
     xy = np.zeros((length, 2))
     xy[0, :] = (x0, 0)
     for n in range(1, length - 1, 2):
-        xy[n, 0] = xy[n - 1, 0]
-        xy[n, 1] = _logistic_map(r, xy[n - 1, 0])
-        xy[n + 1, 0] = xy[n, 1]
-        xy[n + 1, 1] = xy[n, 1]
+        xy[n, :] = [xy[n - 1, 0], _logistic_map(r, xy[n - 1, 0])]
+        xy[n + 1, :] = [xy[n, 1], xy[n, 1]]
     return xy
 
 
 class LogisticMap:
     # pylint: disable=missing-function-docstring
-    # pylint: disable=too-many-instance-attributes
     """The chaotic processes are constructed based on a set of initial
     values (optionally provided or internally generated) and according
     to "logistic map"
@@ -37,17 +29,26 @@ class LogisticMap:
         self._r = r
         self._count = count
         self._length = length
+        self._x0: list
+        self._x: np.ndarray
+        self._xy: np.ndarray
+        self._cobweb: np.ndarray
+        self._construct()
 
-        # processes generated from the logistic map
-        self._x0 = list(np.random.random(count))
+    def _construct(self):
+        self._x0 = list(np.random.random(self._count))
         self._x = np.array([self._x0])
-        self._next(steps=length - 1)
-
-        # the logistic map curve and the cobweb
+        self._compute_sequences(steps=self._length - 1)
         x = np.linspace(0, 1, self._count)
-        y = _logistic_map(r, x)
+        y = _logistic_map(self._r, x)
         self._xy = np.array([x, y]).T
-        self._set_coweb()
+        self._cobweb = _cobweb(self._r, self._x0[0], self._length)
+
+    def _compute_sequences(self, steps):
+        for _ in range(steps):
+            self._x = np.append(
+                self._x, np.atleast_2d(_logistic_map(self._r, self._x[-1, :])), axis=0
+            )
 
     @property
     def r(self):
@@ -57,25 +58,7 @@ class LogisticMap:
     def r(self, r: float):
         assert isinstance(r, float) and 0 <= r <= 4
         self._r = r
-        length = self._x.shape[0]
-        x = np.linspace(0, 1, self._count)
-        y = _logistic_map(r, x)
-        self._xy = np.array([x, y]).T
-        self._set_coweb()
-        self._reset()
-        self._next(steps=length - 1)
-
-    @property
-    def x0(self):
-        return self._x0
-
-    @x0.setter
-    def x0(self, x0: List[float]):
-        if not all(0 <= i <= 1 for i in x0):
-            raise ValueError("Values must always be in [0,1]")
-        self._x0 = x0
-        self._set_coweb()
-        self._reset()
+        self._construct()
 
     @property
     def x(self):
@@ -88,15 +71,3 @@ class LogisticMap:
     @property
     def cobweb(self):
         return self._cobweb
-
-    def _reset(self):
-        self._x = np.array([list(self._x0)])
-
-    def _next(self, steps: int = 1):
-        for _ in range(steps):
-            self._x = np.append(
-                self._x, np.atleast_2d(_logistic_map(self._r, self._x[-1, :])), axis=0
-            )
-
-    def _set_coweb(self):
-        self._cobweb = _cobweb(self._r, self._x0[0], self._length)
