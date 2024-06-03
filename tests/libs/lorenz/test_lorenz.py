7fa41096b41725dd12461f0e67ddc96218221c5f
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2024-06-03 14:00:28 +0200

    Lorenz: add unittest for LorenzParameters class
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- tests/libs/lorenz/test_lorenz.py
+++ tests/libs/lorenz/test_lorenz.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pytest
 
-from libs.lorenz.lorenz import TimeLine
+from libs.lorenz.lorenz import LorenzParameters, TimeLine
 
 
 def test_timeline_initialization():
@@ -34,6 +34,28 @@ def test_timeline_single_value():
     assert timeline.start == 5
     assert timeline.end == 5
     np.testing.assert_array_equal(timeline.times, np.array([5]))
+
+
+def test_lorenz_parameters_initialization():
+    params = LorenzParameters(sigma=10.0, rho=28.0, beta=2.66667)
+    assert params.sigma == 10.0
+    assert params.rho == 28.0
+    assert params.beta == 2.66667
+
+
+@pytest.mark.parametrize(
+    "sigma, rho, beta",
+    [
+        (10.0, 28.0, 2.66667),
+        (14.0, 35.0, 3.0),
+        (8.0, 25.0, 2.5),
+    ],
+)
+def test_lorenz_parameters_values(sigma, rho, beta):
+    params = LorenzParameters(sigma=sigma, rho=rho, beta=beta)
+    assert params.sigma == sigma
+    assert params.rho == rho
+    assert params.beta == beta
 
 
 if __name__ == "__main__":
