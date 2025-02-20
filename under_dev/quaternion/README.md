b72bf2a854d141e4f1f0d7e48493df6cddacb34b
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2025-02-20 15:49:55 +0100

    Dirty-Save: add half-done quaternion
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

---
+++ under_dev/quaternion/README.md
@@ -0,0 +1,31 @@
+# Quaternoin
+
+Rotations with quaternions
+https://imadr.me/rotations-with-quaternions/
+
+A tutorial on SE(3) transformation parameterizations and on-manifold optimization
+https://jinyongjeong.github.io/Download/SE3/jlblanco2010geometry3d_techrep.pdf
+
+and "Exploring SO(3) logarithmic map: degeneracies and derivatives"
+https://vision.in.tum.de/_media/members/demmeln/nurlanov2021so3log.pdf
+
+# Refrences:
+* Plucker Basis Vectors
+  - Roy Featherstone
+  - [link](https://royfeatherstone.org/papers/icra06plucker.pdf)
+* Dual-Quaternions: From Classical Mechanics to Computer Graphics and Beyond
+  - Ben Kenwright
+  - [link](https://xbdev.net/misc_demos/demos/dual_quaternions_beyond/paper.pdf)
+* Spatial Vector and Rigid-Body Dynamics Software
+  - Roy Featherstone
+  - [link](https://royfeatherstone.org/spatial/v2/index.html)
+* A Beginner’s Guide to 6-D Vectors (Part 1)
+  - Roy Featherstone
+  - [link](http://bleyer.org/files/A%20Beginner%27s%20Guide%20to%206-D%20Vectors%20-%20Feathersone%20(IEEE,%202010).pdf)
+* A Beginner’s Guide to 6-D Vectors (Part 2)
+  - Roy Featherstone
+  - [link](???)
+* Rigid Body Dynamics Algorithms
+  - Roy Featherstone
+  - [link](https://link.springer.com/book/10.1007/978-1-4899-7560-7)
+* ChatGPT[3]
---
+++ under_dev/quaternion/main.py
@@ -0,0 +1,79 @@
+from quaternion import (
+    apply_transform,
+    compose_dual_quaternion,
+    decompose_dual_quaternion_to_pose,
+    dual_quaternion_interpolation,
+    dual_quaternion_multiply,
+    euler_to_quat,
+    interpolate_orientation_slerp,
+    interpolate_position_linear,
+    normalize_quaternion,
+)
+from visualization import draw_poses
+
+
+def print_pose(poses):
+    for title, (position, orientation) in poses:
+        print(f"\n{title}")
+        print(f"\tPosition (or translation): {position}")
+        print(f"\tOrientation (Quaternion): {orientation}")
+
+
+# TODO: write unit test
+
+# Issue/bug:
+# * when: start_orientation = euler_to_quat([0, 0, 0])
+# * then: the rotation of it will not take effect, i.e. the end pose will have the same orientation
+# * then: also the plotting fails to plot it! is it a but in quiver?
+
+# Issue/bug:
+# * when: rotation_quaternion = euler_to_quat([0, 90, 180])
+# * then: the interpolation is not smooth! there is a jump in the middle
+
+# Issue/bug:
+# * The whole dual quaternion stuff doesn't seem to work.
+
+start_position = [1.0, 1.0, 1.0]
+start_orientation = euler_to_quat([1, 0, 0])
+start_pose = [start_position, start_orientation]
+
+translation_vector = [1, 1, 1]
+rotation_quaternion = euler_to_quat([45, 45, 90])
+
+mode = ["quaternion", "dual_quaternion"][0]
+interpolate = False
+interpolate_steps = 10
+
+if mode == "quaternion":
+    end_position, end_orientation = apply_transform(
+        start_position, start_orientation, translation_vector, rotation_quaternion
+    )
+    end_pose = [end_position, end_orientation]
+    poses = [start_pose, end_pose]
+
+    if interpolate:
+        orientations = interpolate_orientation_slerp(
+            start_orientation, end_orientation, interpolate_steps
+        )
+        positions = interpolate_position_linear(start_position, end_position, interpolate_steps)
+        poses = [[p, o] for p, o in zip(positions, orientations)]
+
+
+if mode == "dual_quaternion":
+    start_pose_dq = compose_dual_quaternion(*start_pose)
+    transformation_dq = compose_dual_quaternion(translation_vector, rotation_quaternion)
+    end_pose_dq = dual_quaternion_multiply(transformation_dq, start_pose_dq)
+    end_pose_ = decompose_dual_quaternion_to_pose(end_pose_dq)
+    poses = [start_pose, end_pose_]
+    if interpolate:
+        poses = []
+        for i in range(1, interpolate_steps + 1):
+            step = i / (interpolate_steps + 1.0)
+            orientation, position = dual_quaternion_interpolation(
+                start_pose_dq, end_pose_dq, interpolate_steps
+            )
+            poses.append([position, orientation])
+
+
+# starts from blue, and moves to red
+draw_poses(poses)
